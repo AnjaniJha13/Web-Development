9bddad2ab9a6d105ebb408438ed9d87bb0e07f5e
Anjani Jha <157398509+AnjaniJha13@users.noreply.github.com>
2024-12-26 09:59:31 +0530

    Add it to javascript

---
+++ Insert&Removal.js
@@ -0,0 +1,44 @@
+document.querySelector(".container").innerText
+
+document.querySelector(".container").outerHTML
+
+document.querySelector(".container").innerHTML
+
+document.querySelector(".container").tagName
+
+document.querySelector(".container").nodeName
+
+document.querySelector(".container").textContent
+
+document.querySelector(".container").Hidden
+
+document.querySelector(".container").hidden = "true"
+
+document.querySelector(".box").innerHTML = "Hey I am hungry"
+
+document.querySelector(".box").hasAttribute("style")
+
+document.querySelector(".box").getAttribute("style")
+
+document.querySelector(".box").setAttribute("style" , "display: inline")
+
+document.querySelector(".box").attributes
+
+document.querySelector(".box").removeAttribute("style")
+
+document.designMode = "on" //To change or design any website
+
+document.querySelector(".box").dataset
+
+document.querySelector(".container")
+
+document.querySelector(".container").classList
+
+document.querySelector(".container").className
+
+document.querySelector(".container").classList.add("Aman")
+
+document.querySelector(".container").classList.remove("Aman")
+
+document.querySelector(".container").classList.toggle("red")
+
---
+++ Nodes.js
@@ -0,0 +1,47 @@
+console.log("Hello World");
+document.body
+document.body.childNotes
+document.body.childNodes
+
+document.body.childNodes[0]
+
+document.body.childNodes[2]
+
+document.body.childNodes[1]
+
+document.body.childNodes[1].childNodes
+
+let cont = document.body.childNodes[1]
+
+cont
+
+cont.firstElementChild
+
+cont.lastElementChild
+
+cont.firstChild
+
+cont.lastElement
+
+cont.lastChild
+
+cont.lastElemntChild.style.color = "red";
+
+cont.lastElementChild.style.color = "red";
+
+cont.lastElementChild.style.backgroundColor = "yellow";
+'yellow';
+
+document.body.firstElementChild;
+
+document.body.firstElementChild.childNodes;
+
+document.body.firstElementChild.children;
+
+document.body.firstElementChild.children[0];
+
+document.body.firstElementChild.children[2].nextElementSibling;
+
+document.body.firstElementChild.children[2].previousElementSibling;
+
+document.body.children[1].rows
